c28e618ac7f1671f822eae59ca90cd5c56cd91ee
Dmytro <ccccuxoy@gmail.com>
2022-04-13 14:20:26 +0300

    add loops

--- js/script.js
+++ js/script.js
@@ -9,13 +9,56 @@ let personalMuvieDB = {
     genres: [],
     privat: false
 };
+if (personalMuvieDB.count > 0 && personalMuvieDB.count < 10) {
+    alert('not many');
+} else if (10 <= personalMuvieDB.count && personalMuvieDB.count < 30) {
+    alert('good job');
+} else if (personalMuvieDB.count >= 30) {
+    alert('kinoman');
+} else {
+    alert('arror');
+}
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt(' last movie have you seen', ''),
+//           b = +prompt(' rate last movie have you seen', '');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMuvieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
 
-const a = prompt(' last movie have you seen', ''),
-    b = +prompt(' rate last movie have you seen', ''),
-    c = prompt(' last movie have you seen', ''),
-    d = +prompt(' rate last movie have you seen', '');
+//     }
+// }
 
-personalMuvieDB.movies[a] = b;
-personalMuvieDB.movies[c] = d;
+
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt(' last movie have you seen', ''),
+//         b = +prompt(' rate last movie have you seen', '');
+//     i++;
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMuvieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+
+//     }
+// }
+let i = 0;
+do {
+    const a = prompt(' last movie have you seen', ''),
+          b = +prompt(' rate last movie have you seen', '');
+    i++;
+}
+while(i<2);
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMuvieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
 
 console.log(personalMuvieDB);
